cadf999abb5b77b331b4647287caee17aab1c60a
Aubin <aubinseptier@gmail.com>
2022-05-11 19:58:46 +0200

    Token est désormais caché grâce au gitignore.

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
 import discord
+import os
+import annonces
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print("Je suis prêt")
 
 
-
-
-@client.event
-async def on_message(message):
-
-    if message.content.lower() == "quoi":
-        await message.channel.send("feur")
-
-    if message.content.lower() == "oui":
-        await message.channel.send("stiti")
-
-    if message.content.lower() == "wesh":
-        await message.channel.send("dene")
-
-    if message.content.lower() == "ouai":
-        await message.channel.send("stern")
-
-    if message.content.lower() == "comment":
-        await message.channel.send("aire")
-
-    if message.content.lower() == "non":
-        await message.channel.send("bril")
-
-    if message.content.lower() == "ping":
-        await message.channel.send("pong")
-
-    if message.content.lower() == "Je suis trop fort":
-        await message.channel.send("Oui c'est ça")
-
-client.run("OTcwNjgyODE4MDM4NDg5MTI5.Ym_g0g.61lvP7Cj0O1cCEy-OvCMK5i_qTs")
+client.run(os.getenv('DISCORD_TOKEN'))
