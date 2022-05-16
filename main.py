bfa46664b221a2eb9d29ca075c615ddd1a978d61
thomasblond <thomas.blond21@gmail.com>
2022-05-16 15:39:48 +0200

    Initialisation et privatisation du BOT

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="config")
 
-client = discord.Client()
+
+class AumasBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="!")
+
+    async def on_ready(self):
+        print(f"{self.user.display.name} est prêt !")
 
 
-@client.event
-async def on_ready():
-    print("Je suis prêt")
-
-
-client.run(os.getenv("TOKEN"))
+#aumas_bot = AumasBot
+#AumasBot.run(os.getenv("TOKEN"))
+AumasBot.run("OTcwNjgyODE4MDM4NDg5MTI5.GG67vG.xuCNUm8DV0Jtxs7nMQftH7Ud_BL-EgugURhRm4")
